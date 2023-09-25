664a1094d37384a858cd4c05c4d5d90927a41895
Mohamed  Ramadan <74499373+Mo-rmdone@users.noreply.github.com>
2023-09-25 12:59:37 +0300

    Update Basic SQL.sql

--- Basic SQL/Basic SQL.sql
+++ Basic SQL/Basic SQL.sql
@@ -1,3 +1,5 @@
+/* preview the schema of the database used for all the queries. */
+
 /* LIMIT */
 
 /*try using LIMIT yourself below by writing a query that displays all the data in the occurred_at,
@@ -30,50 +32,19 @@ FROM orders
 ORDER BY total_amt_usd
 LIMIT 20;
 
-/* Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).*/
+/* Write a query that displays the order ID, account ID, and total dollar amount for all the orders, 
+sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).*/
 
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY account_id, total_amt_usd DESC;
 
-/* Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).*/
+/* Now write a query that again displays order ID, account ID, and total dollar amount for each order,
+but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).*/
 
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd DESC, account_id;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
